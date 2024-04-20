987a46114bf13ae60575c0d715ab080fcb14427b
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2024-04-20 14:11:37 -0400

    Initial commit

---
+++ Source/Flocking/FlockingGameModeBase.h
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlockingManager.h"
+#include "FlockingGameModeBase.generated.h"
+
+UCLASS()
+class FLOCKING_API AFlockingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	AFlockingGameModeBase();
+
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* AgentMesh;
+
+	UPROPERTY() UFlockingManager* Manager;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
